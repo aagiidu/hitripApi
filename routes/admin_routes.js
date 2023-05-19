19c80e42be42a8a6f7cfe8cece128e45a9a60598
Altangerel <altangerel.d@ddishtv.mn>
2023-05-19 15:40:02 +0800

    blog apis adding

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const AdminController = require('../controllers/admin_controller');
 
-router.post('/blog/add', UserController.addBlog);
-router.post('/blog/delete', UserController.deleteBlog);
+router.post('/blog/add', AdminController.addBlog);
+router.post('/blog/update', AdminController.updateBlog);
+router.post('/blog/delete', AdminController.deleteBlog);
+router.post('/blog/delete/all', AdminController.deleteAllBlog);
 
 module.exports = router;
